64521ec3d3c7628278fb08729e4482a5a894fbd8
Bin Liu <bernardzlau@gmail.com>
2026-08-02 10:48:41 +0200

    Create sod_16000_1.sh

---
+++ sod_tube/sod_16000_1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+#SBATCH --job-name=sod_16000_1
+#SBATCH --account=uzh8              
+#SBATCH --partition=debug
+#SBATCH --time=00:20:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4         
+#SBATCH --cpus-per-task=72          
+#SBATCH --gpus-per-task=1           
+#SBATCH --uenv=prgenv-gnu
+#SBATCH --view=modules
+#SBATCH --output=sod_16000_1.%j.out
+#SBATCH --error=sod_16000_1.%j.err
+
+set -euo pipefail
+
+
+module load boost cmake cray-mpich fftw gcc gsl hdf5 python cuda
+
+cd /users/binliu/pkdgrav3/
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+
+cd /users/binliu/sod_tube
+
+PKDGRAV3=/users/binliu/pkdgrav3/build/pkdgrav3
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export MPICH_GPU_SUPPORT_ENABLED=1          
+
+srun --cpus-per-task=${SLURM_CPUS_PER_TASK} \
+     "${PKDGRAV3}" ./initial/sod_16000_1.par
